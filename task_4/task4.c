527a5d2b16b376803c5530abf7a60ed90d183830
Aisha-827 <58621422+Aisha-827@users.noreply.github.com>
2019-12-21 02:51:03 +0300

    Update task4.c

--- task_4/task4.c
+++ task_4/task4.c
@@ -26,10 +26,10 @@ double monte(double xmax, double ymax, int nsteps, int seed) {
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int nthreads = 8;
+
 double accumulator = 0;
 int npoints = 0x20000000;
-
+int nthreads = 8;
 void *routine() {
     double delta = monte(1, 1, npoints / nthreads, rand());
     pthread_mutex_lock(&mutex);
@@ -38,15 +38,17 @@ void *routine() {
 }
 
 int main() {
-    pthread_t threads[nthreads];
-    for(int i = 1; i < 0x8000; i++) {
+      for(int i = 1; i < 0x8000; i++) {
+       nthreads = i;
+       pthread_t threads[nthreads];
+       accumulator = 0;
        double t0 = get_time();
        if(npoints % i != 0)
-        continue;
-        for(long long i = 0; i < nthreads; i++)
-            pthread_create(&threads[i], NULL, routine, NULL);  
-        for(long long i = 0; i < nthreads; i++)
-            pthread_join(threads[i], NULL);
+         continue;
+        for(long long j = 0; j < nthreads; j++)
+            pthread_create(&threads[j], NULL, routine, NULL);  
+        for(long long k = 0; k < nthreads; k++)
+            pthread_join(threads[k], NULL);
         printf("%d\t%.05lf\t%lf\n", i, get_time() - t0, accumulator);
         }   
      }
